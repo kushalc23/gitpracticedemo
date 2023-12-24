b2980d8dc9216aa3edc2dcf61ff221709a1715e6
kushal <chakrabartykushal23@gmail.com>
2023-12-24 23:15:38 +0530

    first commit

---
+++ src/gitpractice/gitdemo.java
@@ -0,0 +1,17 @@
+package gitpractice;
+
+public class gitdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		while(i<8)
+		{
+			System.out.println(i);
+			i++;
+			
+		}
+
+	}
+
+}
