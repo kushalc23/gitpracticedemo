e6dbacf9fa1de1e23ad2ac254d3874d277c614be
kushal <chakrabartykushal23@gmail.com>
2023-12-24 23:30:16 +0530

    2nd commit

--- src/gitpractice/gitdemo.java
+++ src/gitpractice/gitdemo.java
@@ -5,9 +5,12 @@ public class gitdemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i=0;
+		int j=5;
 		while(i<8)
 		{
 			System.out.println(i);
+			System.out.println(j);
+			
 			i++;
 			
 		}
